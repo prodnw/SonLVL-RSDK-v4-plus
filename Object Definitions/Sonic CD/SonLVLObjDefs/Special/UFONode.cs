47a9f75ec17f20ead36532aaad027da9fdbf0715
MainMemory <sonicmike2@yahoo.com>
2022-02-05 19:23:07 -0600

    Adding Speed property to UFONode object.

--- Object Definitions/Sonic CD/SonLVLObjDefs/Special/UFONode.cs
+++ Object Definitions/Sonic CD/SonLVLObjDefs/Special/UFONode.cs
@@ -1,4 +1,5 @@
 using SonicRetro.SonLVL.API;
+using System;
 
 namespace SCDObjectDefinitions.Special
 {
@@ -22,11 +23,58 @@ namespace SCDObjectDefinitions.Special
 
 		static readonly PropertySpec[] customProperties = new PropertySpec[]
 		{
-			new PropertySpec("Travel Time", typeof(decimal), "Extended", "The time (in seconds) it takes for the UFO to travel from this node to the next.", 1m, o => o.PropertyValue / 30m, (o, v) => o.PropertyValue = (byte)((decimal)v * 30m))
+			new PropertySpec("Travel Time", typeof(decimal), "Extended", "The time (in seconds) it takes for the UFO to travel from this node to the next.", 1m, GetTravelTime, SetTravelTime),
+			new PropertySpec("Speed", typeof(double), "Extended", "The speed at which the UFO moves after this node is reached, in pixels per second.", 30m, GetSpeed, SetSpeed)
 		};
 
 		public override PropertySpec[] CustomProperties { get { return customProperties; } }
 
 		public override byte DefaultSubtype { get { return 30; } }
+
+		static object GetTravelTime(ObjectEntry obj)
+		{
+			return obj.PropertyValue / 30m;
+		}
+
+		static void SetTravelTime(ObjectEntry obj, object value)
+		{
+			decimal dval = (decimal)value;
+			obj.PropertyValue = (byte)Math.Max(Math.Min(dval * 30m, 255), 0);
+		}
+
+		static ObjectEntry GetNextNode(ObjectEntry obj)
+		{
+			int ind = LevelData.Objects.IndexOf(obj);
+			ObjectEntry next = null;
+			if (ind == LevelData.Objects.Count - 1 || LevelData.Objects[ind + 1].Name != "UFO Node")
+			{
+				for (ind--; ind >= 0; --ind)
+					if (LevelData.Objects[ind].Name == "UFO Node")
+						next = LevelData.Objects[ind];
+					else
+						break;
+			}
+			else
+				next = LevelData.Objects[ind + 1];
+			return next;
+		}
+
+		static object GetSpeed(ObjectEntry obj)
+		{
+			double time = obj.PropertyValue / 30d;
+			if (time == 0) return 0d;
+			ObjectEntry next = GetNextNode(obj);
+			if (next == null) return 0d;
+			return Math.Sqrt(Math.Pow(obj.X - next.X, 2) + Math.Pow(obj.Y - next.Y, 2)) / time;
+		}
+
+		static void SetSpeed(ObjectEntry obj, object value)
+		{
+			double dval = (double)value;
+			if (dval == 0) return;
+			ObjectEntry next = GetNextNode(obj);
+			if (next == null) return;
+			obj.PropertyValue = (byte)Math.Max(Math.Min(Math.Sqrt(Math.Pow(obj.X - next.X, 2) + Math.Pow(obj.Y - next.Y, 2)) / dval * 30d, 255), 0);
+		}
 	}
 }

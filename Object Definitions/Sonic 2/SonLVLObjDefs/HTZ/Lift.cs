96aef9a5b1c34ab5c5046a77bf4a98ec8707f95a
Lave sIime <70488825+Lavesiime@users.noreply.github.com>
2022-09-05 15:36:40 -0500

    Small HTZ renders

---
+++ Object Definitions/Sonic 2/SonLVLObjDefs/HTZ/Lift.cs
@@ -0,0 +1,106 @@
+using SonicRetro.SonLVL.API;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Drawing;
+
+namespace S2ObjectDefinitions.HTZ
+{
+	class Lift : ObjectDefinition
+	{
+		private PropertySpec[] properties;
+		private readonly Sprite[] sprites = new Sprite[2];
+		
+		public override ReadOnlyCollection<byte> Subtypes
+		{
+			get { return new ReadOnlyCollection<byte>(new List<byte>()); }
+		}
+
+		public override void Init(ObjectData data)
+		{
+			if (LevelData.StageInfo.folder[LevelData.StageInfo.folder.Length-1] == '5')
+			{
+				BitmapBits sheet = LevelData.GetSpriteSheet("HTZ/Objects.gif");
+				sprites[0] = new Sprite(sheet.GetSection(102, 1, 56, 90), -28, -63);
+				sprites[1] = new Sprite(sheet.GetSection(109, 212, 64, 21), -32, 27);
+			}
+			else
+			{
+				BitmapBits sheet = LevelData.GetSpriteSheet("MBZ/Objects.gif");
+				sprites[0] = new Sprite(sheet.GetSection(73, 848, 56, 90), -28, -63);
+				sprites[1] = new Sprite(sheet.GetSection(1, 953, 64, 21), -32, 27);
+			}
+			
+			properties = new PropertySpec[2];
+			properties[0] = new PropertySpec("Distance", typeof(int), "Extended",
+				"How far this Lift will go.", null,
+				(obj) => obj.PropertyValue,
+				(obj, value) => obj.PropertyValue = (byte)((int)value));
+			
+			properties[1] = new PropertySpec("Travel Direction", typeof(int), "Extended",
+				"Which way the Lift will go.", null, new Dictionary<string, int>
+				{
+					{ "Right", 0 },
+					{ "Left", 1 }
+				},
+				(obj) => (((V4ObjectEntry)obj).Direction.HasFlag(RSDKv3_4.Tiles128x128.Block.Tile.Directions.FlipX)) ? 1 : 0,
+				(obj, value) => ((V4ObjectEntry)obj).Direction = (RSDKv3_4.Tiles128x128.Block.Tile.Directions)value);
+		}
+		
+		public override byte DefaultSubtype
+		{
+			get { return 30; }
+		}
+		
+		public override PropertySpec[] CustomProperties
+		{
+			get { return properties; }
+		}
+
+		public override string SubtypeName(byte subtype)
+		{
+			return null;
+		}
+
+		public override Sprite Image
+		{
+			get { return SubtypeImage(0); }
+		}
+
+		public override Sprite SubtypeImage(byte subtype)
+		{
+			List<Sprite> sprs = new List<Sprite>();
+			for (int i = 0; i < 2; i++)
+			{
+				Sprite sprite = new Sprite(sprites[i]);
+				sprs.Add(sprite);
+			}
+			return new Sprite(sprs.ToArray());
+		}
+
+		public override Sprite GetSprite(ObjectEntry obj)
+		{
+			List<Sprite> sprs = new List<Sprite>();
+			for (int i = 0; i < 2; i++)
+			{
+				Sprite sprite = new Sprite(sprites[i]);
+				sprite.Flip((((V4ObjectEntry)obj).Direction.HasFlag(RSDKv3_4.Tiles128x128.Block.Tile.Directions.FlipX)), false);
+				sprs.Add(sprite);
+			}
+			return new Sprite(sprs.ToArray());
+		}
+		
+		public override Sprite GetDebugOverlay(ObjectEntry obj)
+		{
+			int dist = obj.PropertyValue << 3;
+			
+			int sx = dist * 2;
+			int sy = dist * 1;
+			
+			var bitmap = new BitmapBits(sx + 1, sy + 1);
+			bitmap.DrawLine(LevelData.ColorWhite, 0, 0, sx, sy);
+			bitmap.Flip((((V4ObjectEntry)obj).Direction.HasFlag(RSDKv3_4.Tiles128x128.Block.Tile.Directions.FlipX)), false);
+			return new Sprite(bitmap, (((V4ObjectEntry)obj).Direction.HasFlag(RSDKv3_4.Tiles128x128.Block.Tile.Directions.FlipX)) ? -sx : 0, 0);
+		}
+	}
+}

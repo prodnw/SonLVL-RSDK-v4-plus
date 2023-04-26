bd96993f69aee659d1459b63595e447ced998c87
Lave sIime <70488825+Lavesiime@users.noreply.github.com>
2023-04-25 19:31:36 -0500

    oops didn't finish that

--- Project Files/Sonic 1/SonLVLObjDefs/Enemies/Caterkiller.cs
+++ Project Files/Sonic 1/SonLVLObjDefs/Enemies/Caterkiller.cs
@@ -7,49 +7,64 @@ namespace S1ObjectDefinitions.Enemies
 {
 	class Caterkiller : ObjectDefinition
 	{
-		private readonly Sprite[] sprites = new Sprite[3];
-		private PropertySpec[] properties;
+		private readonly Sprite[] sprites = new Sprite[2];
+		private PropertySpec[] properties = new PropertySpec[1];
 
 		public override void Init(ObjectData data)
 		{
+			Sprite[] frames = new Sprite[3];
 			switch (LevelData.StageInfo.folder[LevelData.StageInfo.folder.Length-1])
 			{
 				case '2':
 				default:
 					BitmapBits sheet = LevelData.GetSpriteSheet("MZ/Objects.gif");
-					sprites[0] = new Sprite(sheet.GetSection(18, 81, 16, 22), -8, -14);
-					sprites[1] = new Sprite(sheet.GetSection(1, 81, 16, 24), -8, -14);
-					sprites[2] = new Sprite(sheet.GetSection(35, 81, 16, 16), -8, -8);
+					frames[0] = new Sprite(sheet.GetSection(18, 81, 16, 22), -8, -14);
+					frames[1] = new Sprite(sheet.GetSection(1, 81, 16, 24), -8, -14);
+					frames[2] = new Sprite(sheet.GetSection(35, 81, 16, 16), -8, -8);
 					break;
 				case '3':
 					sheet = LevelData.GetSpriteSheet("SYZ/Objects.gif");
-					sprites[0] = new Sprite(sheet.GetSection(98, 98, 16, 22), -8, -14);
-					sprites[1] = new Sprite(sheet.GetSection(81, 98, 16, 24), -8, -14);
-					sprites[2] = new Sprite(sheet.GetSection(98, 121, 16, 16), -8, -8);
+					frames[0] = new Sprite(sheet.GetSection(98, 98, 16, 22), -8, -14);
+					frames[1] = new Sprite(sheet.GetSection(81, 98, 16, 24), -8, -14);
+					frames[2] = new Sprite(sheet.GetSection(98, 121, 16, 16), -8, -8);
 					break;
 				case '6':
 					sheet = LevelData.GetSpriteSheet("SBZ/Objects.gif");
-					sprites[0] = new Sprite(sheet.GetSection(75, 26, 16, 22), -8, -14);
-					sprites[1] = new Sprite(sheet.GetSection(75, 1, 16, 24), -8, -14);
-					sprites[2] = new Sprite(sheet.GetSection(75, 49, 16, 16), -8, -8);
+					frames[0] = new Sprite(sheet.GetSection(75, 26, 16, 22), -8, -14);
+					frames[1] = new Sprite(sheet.GetSection(75, 1, 16, 24), -8, -14);
+					frames[2] = new Sprite(sheet.GetSection(75, 49, 16, 16), -8, -8);
 					break;
 				case '7':
 					sheet = LevelData.GetSpriteSheet("MBZ/Objects.gif");
-					sprites[0] = new Sprite(sheet.GetSection(1, 68, 16, 22), -8, -14);
-					sprites[1] = new Sprite(sheet.GetSection(18, 68, 16, 24), -8, -14);
-					sprites[2] = new Sprite(sheet.GetSection(35, 68, 16, 16), -8, -8);
+					frames[0] = new Sprite(sheet.GetSection(1, 68, 16, 22), -8, -14);
+					frames[1] = new Sprite(sheet.GetSection(18, 68, 16, 24), -8, -14);
+					frames[2] = new Sprite(sheet.GetSection(35, 68, 16, 16), -8, -8);
 					break;
 			}
-
-			properties = new PropertySpec[1];
+			
+			for (int s = 0; s < 2; s++)
+			{
+				List<Sprite> sprs = new List<Sprite>();
+				bool flip = (s != 0);
+				
+				for (int i = 4; i > 0; i--)
+				{
+					Sprite tmp = new Sprite(frames[(i == 1) ? 0 : 2], flip, false);
+					tmp.Offset(((i - 1) * 12) * (flip ? (-1) : (1)), 0);
+					sprs.Add(tmp);
+				}
+				
+				sprites[s] = new Sprite(sprs.ToArray());
+			}
+			
 			properties[0] = new PropertySpec("Direction", typeof(int), "Extended",
 				"Which way the Caterkiller is facing.", null, new Dictionary<string, int>
 				{
 					{ "Left", 0 },
 					{ "Right", 1 }
 				},
-				(obj) => obj.PropertyValue & 1,
-				(obj, value) => obj.PropertyValue = (byte)((obj.PropertyValue & 254) | (byte)((int)value)));
+				(obj) => obj.PropertyValue == 1,
+				(obj, value) => obj.PropertyValue = (byte)((int)value));
 		}
 
 		public override ReadOnlyCollection<byte> Subtypes
@@ -72,39 +87,26 @@ namespace S1ObjectDefinitions.Enemies
 			switch (subtype)
 			{
 				case 0:
+				default:
 					return "Facing Left";
 				case 1:
 					return "Facing Right";
-				default:
-					return "Unknown";
 			}
 		}
 
 		public override Sprite Image
 		{
-			get { return SubtypeImage(0); }
+			get { return sprites[0]; }
 		}
 
 		public override Sprite SubtypeImage(byte subtype)
 		{
-			List<Sprite> sprs = new List<Sprite>();
-			
-			bool flip = (subtype & 1) > 0;
-			
-			for (int i = 4; i > 0; i--)
-			{
-				Sprite tmp = new Sprite(sprites[(i == 1) ? 0 : 2]);
-				tmp.Flip(flip, false);
-				tmp.Offset(((i - 1) * 12) * (flip ? (-1) : (1)), 0);
-				sprs.Add(tmp);
-			}
-			
-			return new Sprite(sprs.ToArray());
+			return sprites[(subtype == 1) ? 1 : 0];
 		}
 
 		public override Sprite GetSprite(ObjectEntry obj)
 		{
-			return SubtypeImage(obj.PropertyValue);
+			return sprites[(obj.PropertyValue == 1) ? 1 : 0];
 		}
 	}
 }

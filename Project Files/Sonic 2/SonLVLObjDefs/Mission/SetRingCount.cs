448282a0ab915f0828339ae30cb567d8526f875a
Lave sIime <70488825+Lavesiime@users.noreply.github.com>
2023-07-03 16:36:15 -0500

    M035 Block Render
    not a good one but it works

--- Project Files/Sonic 2/SonLVLObjDefs/Mission/SetRingCount.cs
+++ Project Files/Sonic 2/SonLVLObjDefs/Mission/SetRingCount.cs
@@ -8,15 +8,18 @@ namespace S2ObjectDefinitions.Mission
 	class SetRingCount : ObjectDefinition
 	{
 		private Sprite sprite;
-		private PropertySpec[] properties;
+		private PropertySpec[] properties = new PropertySpec[1];
 
 		public override void Init(ObjectData data)
 		{
-			sprite = new Sprite(LevelData.GetSpriteSheet("Global/Items.gif").GetSection(1, 1, 16, 16), -8, -8);
-
-			properties = new PropertySpec[1];
+			Sprite frame = new Sprite(LevelData.GetSpriteSheet("Global/Items.gif").GetSection(1, 1, 16, 16), -8, -8);
+			
+			BitmapBits bitmap = new BitmapBits(18, 18);
+			bitmap.DrawRectangle(6, 0, 0, 17, 17); // LevelData.ColorWhite
+			sprite = new Sprite(frame, new Sprite(bitmap, -9, -9));
+			
 			properties[0] = new PropertySpec("Ring Count", typeof(int), "Extended",
-				"How many rings the player will start with.", null,
+				"How many rings the player will start with.", 10,
 				(obj) => ((V4ObjectEntry)obj).Value0,
 				(obj, value) => ((V4ObjectEntry)obj).Value0 = ((int)value));
 		}

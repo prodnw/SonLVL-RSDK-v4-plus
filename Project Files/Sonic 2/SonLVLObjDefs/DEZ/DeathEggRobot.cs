92b6b4806f75af864bc0f87efb1821332f18dc1e
Lave sIime <70488825+Lavesiime@users.noreply.github.com>
2023-12-22 18:27:58 -0600

    Remove some unneeded `DefaultSubtype`s

--- Project Files/Sonic 2/SonLVLObjDefs/DEZ/DeathEggRobot.cs
+++ Project Files/Sonic 2/SonLVLObjDefs/DEZ/DeathEggRobot.cs
@@ -32,11 +32,6 @@ namespace S2ObjectDefinitions.DEZ
 			get { return new ReadOnlyCollection<byte>(new byte[] {0, 1}); }
 		}
 		
-		public override byte DefaultSubtype
-		{
-			get { return 0; }
-		}
-		
 		public override PropertySpec[] CustomProperties
 		{
 			get { return properties; }

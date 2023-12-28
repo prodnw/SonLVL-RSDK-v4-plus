a1c716520cbd3008d2d8bde2d00aa4c75ddf9ce5
Lave sIime <70488825+Lavesiime@users.noreply.github.com>
2023-12-28 16:21:26 -0600

    Add Group of Objects: change default distance to 0

--- SonLVL/AddGroupDialog.Designer.cs
+++ SonLVL/AddGroupDialog.Designer.cs
@@ -28,181 +28,186 @@ namespace SonicRetro.SonLVL.GUI
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.okButton = new System.Windows.Forms.Button();
-			this.cancelButton = new System.Windows.Forms.Button();
-			this.XDist = new System.Windows.Forms.NumericUpDown();
-			this.label1 = new System.Windows.Forms.Label();
-			this.label2 = new System.Windows.Forms.Label();
-			this.YDist = new System.Windows.Forms.NumericUpDown();
-			this.label3 = new System.Windows.Forms.Label();
-			this.Rows = new System.Windows.Forms.NumericUpDown();
-			this.label4 = new System.Windows.Forms.Label();
-			this.Columns = new System.Windows.Forms.NumericUpDown();
-			((System.ComponentModel.ISupportInitialize)(this.XDist)).BeginInit();
-			((System.ComponentModel.ISupportInitialize)(this.YDist)).BeginInit();
-			((System.ComponentModel.ISupportInitialize)(this.Rows)).BeginInit();
-			((System.ComponentModel.ISupportInitialize)(this.Columns)).BeginInit();
-			this.SuspendLayout();
-			// 
-			// okButton
-			// 
-			this.okButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-			this.okButton.DialogResult = System.Windows.Forms.DialogResult.OK;
-			this.okButton.Location = new System.Drawing.Point(11, 120);
-			this.okButton.Name = "okButton";
-			this.okButton.Size = new System.Drawing.Size(75, 23);
-			this.okButton.TabIndex = 0;
-			this.okButton.Text = "&OK";
-			this.okButton.UseVisualStyleBackColor = true;
-			this.okButton.Click += new System.EventHandler(this.okButton_Click);
-			// 
-			// cancelButton
-			// 
-			this.cancelButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-			this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-			this.cancelButton.Location = new System.Drawing.Point(92, 120);
-			this.cancelButton.Name = "cancelButton";
-			this.cancelButton.Size = new System.Drawing.Size(75, 23);
-			this.cancelButton.TabIndex = 1;
-			this.cancelButton.Text = "&Cancel";
-			this.cancelButton.UseVisualStyleBackColor = true;
-			this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
-			// 
-			// XDist
-			// 
-			this.XDist.Hexadecimal = true;
-			this.XDist.Location = new System.Drawing.Point(78, 12);
-			this.XDist.Maximum = new decimal(new int[] {
-			4095,
-			0,
-			0,
-			0});
-			this.XDist.Name = "XDist";
-			this.XDist.Size = new System.Drawing.Size(47, 20);
-			this.XDist.TabIndex = 2;
-			this.XDist.Value = new decimal(new int[] {
-			4095,
-			0,
-			0,
-			0});
-			// 
-			// label1
-			// 
-			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(12, 14);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(60, 13);
-			this.label1.TabIndex = 3;
-			this.label1.Text = "X distance:";
-			// 
-			// label2
-			// 
-			this.label2.AutoSize = true;
-			this.label2.Location = new System.Drawing.Point(12, 40);
-			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(60, 13);
-			this.label2.TabIndex = 5;
-			this.label2.Text = "Y distance:";
-			// 
-			// YDist
-			// 
-			this.YDist.Hexadecimal = true;
-			this.YDist.Location = new System.Drawing.Point(78, 38);
-			this.YDist.Maximum = new decimal(new int[] {
-			4095,
-			0,
-			0,
-			0});
-			this.YDist.Name = "YDist";
-			this.YDist.Size = new System.Drawing.Size(47, 20);
-			this.YDist.TabIndex = 4;
-			this.YDist.Value = new decimal(new int[] {
-			4095,
-			0,
-			0,
-			0});
-			// 
-			// label3
-			// 
-			this.label3.AutoSize = true;
-			this.label3.Location = new System.Drawing.Point(12, 66);
-			this.label3.Name = "label3";
-			this.label3.Size = new System.Drawing.Size(37, 13);
-			this.label3.TabIndex = 7;
-			this.label3.Text = "Rows:";
-			// 
-			// Rows
-			// 
-			this.Rows.Location = new System.Drawing.Point(78, 64);
-			this.Rows.Minimum = new decimal(new int[] {
-			1,
-			0,
-			0,
-			0});
-			this.Rows.Name = "Rows";
-			this.Rows.Size = new System.Drawing.Size(47, 20);
-			this.Rows.TabIndex = 6;
-			this.Rows.Value = new decimal(new int[] {
-			1,
-			0,
-			0,
-			0});
-			// 
-			// label4
-			// 
-			this.label4.AutoSize = true;
-			this.label4.Location = new System.Drawing.Point(12, 92);
-			this.label4.Name = "label4";
-			this.label4.Size = new System.Drawing.Size(50, 13);
-			this.label4.TabIndex = 9;
-			this.label4.Text = "Columns:";
-			// 
-			// Columns
-			// 
-			this.Columns.Location = new System.Drawing.Point(78, 90);
-			this.Columns.Minimum = new decimal(new int[] {
-			1,
-			0,
-			0,
-			0});
-			this.Columns.Name = "Columns";
-			this.Columns.Size = new System.Drawing.Size(47, 20);
-			this.Columns.TabIndex = 8;
-			this.Columns.Value = new decimal(new int[] {
-			1,
-			0,
-			0,
-			0});
-			// 
-			// AddGroupDialog
-			// 
-			this.AcceptButton = this.okButton;
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(179, 155);
-			this.Controls.Add(this.label4);
-			this.Controls.Add(this.Columns);
-			this.Controls.Add(this.label3);
-			this.Controls.Add(this.Rows);
-			this.Controls.Add(this.label2);
-			this.Controls.Add(this.YDist);
-			this.Controls.Add(this.label1);
-			this.Controls.Add(this.XDist);
-			this.Controls.Add(this.cancelButton);
-			this.Controls.Add(this.okButton);
-			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-			this.MaximizeBox = false;
-			this.MinimizeBox = false;
-			this.Name = "AddGroupDialog";
-			this.ShowIcon = false;
-			this.ShowInTaskbar = false;
-			this.Text = "AddGroupDialog";
-			((System.ComponentModel.ISupportInitialize)(this.XDist)).EndInit();
-			((System.ComponentModel.ISupportInitialize)(this.YDist)).EndInit();
-			((System.ComponentModel.ISupportInitialize)(this.Rows)).EndInit();
-			((System.ComponentModel.ISupportInitialize)(this.Columns)).EndInit();
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            this.okButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.XDist = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.YDist = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Rows = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Columns = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.XDist)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.YDist)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Rows)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Columns)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // okButton
+            // 
+            this.okButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.okButton.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.okButton.Location = new System.Drawing.Point(15, 148);
+            this.okButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(100, 28);
+            this.okButton.TabIndex = 0;
+            this.okButton.Text = "&OK";
+            this.okButton.UseVisualStyleBackColor = true;
+            this.okButton.Click += new System.EventHandler(this.okButton_Click);
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.cancelButton.Location = new System.Drawing.Point(123, 148);
+            this.cancelButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(100, 28);
+            this.cancelButton.TabIndex = 1;
+            this.cancelButton.Text = "&Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+            // 
+            // XDist
+            // 
+            this.XDist.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.XDist.Hexadecimal = true;
+            this.XDist.Location = new System.Drawing.Point(104, 15);
+            this.XDist.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.XDist.Maximum = new decimal(new int[] {
+            4095,
+            0,
+            0,
+            0});
+            this.XDist.Name = "XDist";
+            this.XDist.Size = new System.Drawing.Size(63, 22);
+            this.XDist.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 17);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "X distance:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 49);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Y distance:";
+            // 
+            // YDist
+            // 
+            this.YDist.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.YDist.Hexadecimal = true;
+            this.YDist.Location = new System.Drawing.Point(104, 47);
+            this.YDist.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.YDist.Maximum = new decimal(new int[] {
+            4095,
+            0,
+            0,
+            0});
+            this.YDist.Name = "YDist";
+            this.YDist.Size = new System.Drawing.Size(63, 22);
+            this.YDist.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 81);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Rows:";
+            // 
+            // Rows
+            // 
+            this.Rows.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Rows.Location = new System.Drawing.Point(104, 79);
+            this.Rows.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Rows.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.Rows.Name = "Rows";
+            this.Rows.Size = new System.Drawing.Size(63, 22);
+            this.Rows.TabIndex = 6;
+            this.Rows.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 113);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Columns:";
+            // 
+            // Columns
+            // 
+            this.Columns.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Columns.Location = new System.Drawing.Point(104, 111);
+            this.Columns.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Columns.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.Columns.Name = "Columns";
+            this.Columns.Size = new System.Drawing.Size(63, 22);
+            this.Columns.TabIndex = 8;
+            this.Columns.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // AddGroupDialog
+            // 
+            this.AcceptButton = this.okButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(239, 191);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.Columns);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Rows);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.YDist);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.XDist);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.okButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "AddGroupDialog";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "AddGroupDialog";
+            ((System.ComponentModel.ISupportInitialize)(this.XDist)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.YDist)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Rows)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Columns)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
 		}
 

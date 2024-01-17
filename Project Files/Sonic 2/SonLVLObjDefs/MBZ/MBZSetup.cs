ab9d5e34ef63c98c8e0e307b617368c4b15ea0b7
Lave sIime <70488825+Lavesiime@users.noreply.github.com>
2024-01-16 22:39:04 -0600

    MBZ Palette Tweak
    this one's a bit iffy..
    some info: MBZ's tileset palette normally has an orange in place of black, which makes the stage look kinda weird, this changes it so that it's now a black, but at the same time should an object definition really be actually changing the stage, right? but at the same time i feel like it's kind of reasonable here, even if it is admittedly not the best practice...

--- Project Files/Sonic 2/SonLVLObjDefs/MBZ/MBZSetup.cs
+++ Project Files/Sonic 2/SonLVLObjDefs/MBZ/MBZSetup.cs
@@ -9,8 +9,13 @@ namespace S2ObjectDefinitions.MBZ
 		public override void Init(ObjectData data)
 		{
 			// MBZ's tile palette uses an orange for boss blacks while in-game uses, well, black, so
-			// we do a little orange->black correction here, only affects object panel and not actual palette
-			LevelData.BmpPal.Entries[192] = Color.Black;
+			// (let's just do a little correction here, only set it if the current colour there is the same orange as in the original palette)
+			// (yeah i'm iffy on this because an obj def shouldn't really be editing the stage, but i feel like it's kind of warranted here? not sure yet, may change this later..)
+			if (LevelData.BmpPal.Entries[192] == Color.FromArgb(192, 96, 0))
+			{
+				LevelData.BmpPal.Entries[192] = Color.Black;
+				LevelData.NewPalette[192] = Color.Black;
+			}
 			
 			base.Init(data);
 		}

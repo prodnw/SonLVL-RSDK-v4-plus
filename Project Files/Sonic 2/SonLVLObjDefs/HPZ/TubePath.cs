0894594bab0034678568079e2e3b8d7f8a39fb67
Lave sIime <70488825+Lavesiime@users.noreply.github.com>
2024-10-28 02:29:29 -0500

    S2 Tube Path tweak
    now there's a lil arrow at the end! \:D

--- Project Files/Sonic 2/SonLVLObjDefs/HPZ/TubePath.cs
+++ Project Files/Sonic 2/SonLVLObjDefs/HPZ/TubePath.cs
@@ -4,16 +4,32 @@ using System.Linq;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 
+namespace S2ObjectDefinitions.MBZ
+{
+	class TubePath : S2ObjectDefinitions.HPZ.TubePath
+	{
+		public override double angle { get { return 1.5; } }
+		public override int length { get { return 104; } }
+	}
+}
+
 namespace S2ObjectDefinitions.HPZ
 {
 	class TubePath : ObjectDefinition
 	{
 		private PropertySpec[] properties = new PropertySpec[1];
+		private BitmapBits arrow;
 		private Sprite sprite;
+		
+		public virtual double angle { get { return 0.0; } }
+		public virtual int length { get { return 36; } }
 		
 		public override void Init(ObjectData data)
 		{
 			sprite = new Sprite(LevelData.GetSpriteSheet("Global/Display.gif").GetSection(168, 18, 16, 16), -8, -8);
+			
+			arrow = new BitmapBits(length * 2, length * 2);
+			arrow.DrawArrow(6, length, length, length + (int)(Math.Cos(angle * Math.PI) * (length - 1)), length + (int)(Math.Sin(angle * Math.PI) * (length - 1)));
 			
 			properties[0] = new PropertySpec("Marker Type", typeof(int), "Extended",
 				"If this Tube Path is an entrance or an exit.", null, new Dictionary<string, int>
@@ -71,12 +87,28 @@ namespace S2ObjectDefinitions.HPZ
 		public override Sprite GetDebugOverlay(ObjectEntry obj)
 		{
 			if (obj.PropertyValue != 0)
+			{
+				for (int i = LevelData.Objects.IndexOf(obj) - 1; i >= 0; --i)
+				{
+					switch (LevelData.Objects[i].Name)
+					{
+						case "Tube Path":
+							if (LevelData.Objects[i].PropertyValue == 0) LevelData.Objects[i].UpdateDebugOverlay();
+							break;
+						case "Blank Object":
+							break;
+						default:
+							return null;
+					}
+				}
+				
 				return null;
+			}
 			
 			try
 			{
-				// p much copied directly from SCD UFO but hey, if it works it works
 				// Do note, in-game behaviour doesn't enforce that the path ends with a Tube Path exit - it just has to be a non-zero obj type
+				// though, if you have it set up wrong, then Sonic will just never gain control again and just be stuck intangible forever..
 				
 				List<ObjectEntry> nodes = LevelData.Objects.Skip(LevelData.Objects.IndexOf(obj) + 1).TakeWhile(a => a.Name == "Blank Object").ToList();
 				if (nodes.Count == 0)
@@ -108,13 +140,27 @@ namespace S2ObjectDefinitions.HPZ
 					black.DrawLine(1, nodes[i].X - xmin + 1, nodes[i].Y - ymin + 1, nodes[i + 1].X - xmin + 1, nodes[i + 1].Y - ymin + 1);
 				}
 				
-				return new Sprite(new Sprite(black, xmin - obj.X, ymin - obj.Y), new Sprite(white, xmin - obj.X, ymin - obj.Y));
+				return new Sprite(new Sprite(black, xmin - obj.X, ymin - obj.Y),
+								  new Sprite(white, xmin - obj.X, ymin - obj.Y),
+								  new Sprite(arrow, nodes.Last().X - obj.X - length, nodes.Last().Y - obj.Y - length));
 			}
 			catch
 			{
 			}
 			
 			return null;
+		}
+	}
+	
+	public static class BitmapBitsExtensions
+	{
+		public static void DrawArrow(this BitmapBits bitmap, byte index, int x1, int y1, int x2, int y2)
+		{
+			bitmap.DrawLine(index, x1, y1, x2, y2);
+			
+			double angle = Math.Atan2(y1 - y2, x1 - x2);
+			bitmap.DrawLine(index, x2, y2, x2 + (int)(Math.Cos(angle + 0.60) * 10), y2 + (int)(Math.Sin(angle + 0.60) * 10));
+			bitmap.DrawLine(index, x2, y2, x2 + (int)(Math.Cos(angle - 0.60) * 10), y2 + (int)(Math.Sin(angle - 0.60) * 10));
 		}
 	}
 }
